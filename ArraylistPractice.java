55c8b3a36a81423e6654fd0df1205c3282ba8f9a
Ritam Maty <ritammaty2006@example.com>
2025-10-28 01:00:51 +0530

    testing all the fun of arraylist

---
+++ ArraylistPractice.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+public class ArraylistPractice{
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        /**================================================================================================
+         *!                                        testing all of the funcaiton
+         *================================================================================================**/
+        // System.out.println("checking the number 70 contain or not : "+ list.contains(70));
+        // System.out.println(list.remove(0)+" removed from the list");
+        // System.out.println(list);
+        // System.out.println("the first element of the list is :"+(list.get(0)));
+        // list.set(0,100);
+        // System.out.println(list);
+        
+    
+    }
+}
